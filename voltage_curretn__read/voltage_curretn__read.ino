7408eb9b0719cdca0095718d9949101d84c20eaa
Sagar <koiralasamir633@gmail.com>
2021-08-10 19:44:58 +0545

    current reading added to file voltage_current_read

--- voltage_curretn__read/voltage_curretn__read.ino
+++ voltage_curretn__read/voltage_curretn__read.ino
@@ -1,12 +1,12 @@
-#define sp_volt_pin A4
-#define sp_current_pin  A1
 #define batt_volt_pin A5
-#define batt_current_pin A4
+#define sp_volt_pin A4
+#define batt_current_pin A1
+#define sp_current_pin  A0
 
 float sp_volt = 0;
 float sp_current = 0;
 float batt_volt = 0;
-float batt_current = 0;
+int batt_current = 0;
 
 void setup() {
   pinMode(sp_volt_pin, INPUT);
@@ -20,11 +20,17 @@ void setup() {
 void loop() {
   batt_volt = map(analogRead(batt_volt_pin), 0, 654, 0, 420)/100.0;
   Serial.print("Battery_Volt: ");
-  Serial.println(batt_volt);
+  Serial.print(batt_volt);
+  Serial.println("V");
   sp_volt = map(analogRead(sp_volt_pin), 0, 930, 0, 600)/100.0;
   Serial.print("SolarPanel_Volt: ");
-  Serial.println(sp_volt);
+  Serial.print(sp_volt);
+  Serial.println("V");
+  batt_current = map(analogRead(batt_current_pin), 0, 954, 0, 5000);
+  Serial.print("Current_consumed: ");
+  Serial.print(batt_current);
+  Serial.println("mA");
   Serial.println();
   delay(100);
-
+  
 }
